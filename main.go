21b996c44ac52c8629ece622151a1f85798be3e0
David Gonzalez <davidguibregon@gmail.com>
2024-10-08 14:53:44 -0500

    feat: :sparkles: new FileServer

--- main.go
+++ main.go
@@ -2,65 +2,37 @@ package main
 
 import (
     "log"
-    "net"
-    "os"
-    "time"
+	"net"
+	"net/http"
+	"time"
 
     pb "github.com/Districorp-UPB/FileServer/proto"
     "github.com/Districorp-UPB/FileServer/server"
     "google.golang.org/grpc"
-    "google.golang.org/grpc/grpclog"
-    "google.golang.org/grpc/keepalive"
 )
 
-func init() {
-    // Configurar el logger de gRPC
-    grpclog.SetLoggerV2(grpclog.NewLoggerV2WithVerbosity(os.Stdout, os.Stdout, os.Stderr, 4))
-}
-
 func main() {
-    log.Println("Iniciando la aplicación...")
+	// Servidor gRPC
+	grpcListener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer grpcListener.Close()
 
-    grpc.EnableTracing = true
 
-    // Servidor gRPC
-    grpcListener, err := net.Listen("tcp", "0.0.0.0:50051")
-    if err != nil {
-        log.Fatalf("Error crítico: no se pudo escuchar en el puerto 50051: %v", err)
-    }
-    log.Println("Listener de gRPC creado, esperando conexiones...")
-    defer func() {
-        log.Println("Cerrando el listener de gRPC...")
-        if err := grpcListener.Close(); err != nil {
-            log.Fatalf("Error al cerrar el listener: %v", err)
-        }
-        log.Println("Listener cerrado correctamente.")
-    }()
+	// Iniciar servidor gRPC con timeout de 5 minutos y limite de 1GB
+	grpcServer := grpc.NewServer(
+		grpc.MaxRecvMsgSize(1024*1024*1024),
+		grpc.ConnectionTimeout(time.Minute*5),
+	)
+	pb.RegisterFileServiceServer(grpcServer, &server.FileService{})
+	log.Println("gRPC server started")
+	go func() {
+		if err := grpcServer.Serve(grpcListener); err != nil {
+			log.Fatalf("failed to serve gRPC: %v", err)
+		}
+	}()
 
-    // Configuración de keepalive
-    kasp := keepalive.ServerParameters{
-        MaxConnectionIdle:     15 * time.Minute,
-        MaxConnectionAge:      30 * time.Minute,
-        MaxConnectionAgeGrace: 5 * time.Minute,
-        Time:                  5 * time.Minute,
-        Timeout:               1 * time.Minute,
-    }
-
-    // Iniciar servidor gRPC con timeout de 10 minutos y límite de 1GB
-    grpcServer := grpc.NewServer(
-        grpc.MaxRecvMsgSize(1024*1024*1024),
-        grpc.ConnectionTimeout(time.Minute*10),
-        grpc.KeepaliveParams(kasp),
-    )
-
-    pb.RegisterFileServiceServer(grpcServer, &server.FileService{})
-    log.Println("Servidor gRPC iniciado en el puerto 50051.")
-
-    // Ejecutar el servidor
-    log.Println("Ejecutando el servidor gRPC...")
-    if err := grpcServer.Serve(grpcListener); err != nil {
-        log.Fatalf("Error crítico: fallo al servir gRPC: %v", err)
-    }
-
-    log.Println("Servidor gRPC detenido.")
+	
 }
+
--- server/uploadServer.go
+++ server/uploadServer.go
@@ -2,97 +2,86 @@ package server
 
 import (
     "encoding/base64"
-    "fmt"
-    "log"
-    "os"
-    "path/filepath"
-    "io"
+	"fmt"
+	"os"
+	"path/filepath"
 
     pb "github.com/Districorp-UPB/FileServer/proto"
 )
 
 type FileService struct {
-    pb.UnimplementedFileServiceServer
+	pb.UnimplementedFileServiceServer
 }
 
 func (s *FileService) Upload(stream pb.FileService_UploadServer) error {
-    log.Println("Comenzando la carga del archivo...")
+	// Leer el request desde el flujo de datos
+	req, err := stream.Recv()
+	if err != nil {
+		return fmt.Errorf("failed to receive upload request: %w", err)
+	}
 
-    var fileId string
+	// Subir archivo
+	uploadToNFS(req)
+	if err != nil {
+		return fmt.Errorf("failed to upload file to NFS: %w", err)
+	}
 
-    for {
-        req, err := stream.Recv()
-        if err != nil {
-            if err == io.EOF {
-                log.Println("Fin de la transmisión recibido.")
-                break
-            }
-            return fmt.Errorf("failed to receive upload request: %w", err)
-        }
 
-        log.Printf("Recibido chunk del archivo con ID: %s de tamaño %d bytes\n", req.FileId, len(req.BinaryFile))
+	// Respuesta
+	err = stream.SendAndClose(&pb.FileUploadResponse{
+		FileId: req.FileId,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send upload response: %w", err)
+	}
 
-        fileId = req.FileId
-
-        _, err = uploadToNFS(req)
-        if err != nil {
-            log.Printf("Error al subir archivo a NFS: %v", err)
-            return fmt.Errorf("failed to upload file to NFS: %w", err)
-        }
-    }
-
-    log.Println("Preparando la respuesta para el cliente...")
-    err := stream.SendAndClose(&pb.FileUploadResponse{
-        FileId: fileId,
-    })
-    if err != nil {
-        log.Printf("Error al enviar la respuesta: %v", err)
-        return fmt.Errorf("failed to send upload response: %w", err)
-    }
-
-    log.Println("Archivo subido exitosamente.")
-    return nil
+	return nil
 }
 
+
 func uploadToNFS(req *pb.FileUploadRequest) (string, error) {
-    userPath := fmt.Sprintf("./nfs/files/%s", req.OwnerId)
-    if _, err := os.Stat(userPath); os.IsNotExist(err) {
-        err := os.Mkdir(userPath, 0755)
-        if err != nil {
-            return "", fmt.Errorf("failed to create user directory: %w", err)
-        }
-        log.Printf("Directorio creado para el usuario: %s", req.OwnerId)
-    }
+	// Si el usuario nunca ha subido un archivo, crear un directorio para el usuario
+	userPath := fmt.Sprintf("./nfs/files/%s", req.OwnerId)
+	if _, err := os.Stat(userPath); os.IsNotExist(err) {
+		err := os.Mkdir(userPath, 0755)
+		if err != nil {
+			return "", fmt.Errorf("failed to create user directory: %w", err)
+		}
+	}
 
-    fileExtension := filepath.Ext(req.FileName)
-    fileName := req.FileId + fileExtension
+	// Extraer la extensión y el nombre del archivo
+	fileExtension := filepath.Ext(req.FileName)
+	fileName := req.FileId + fileExtension
 
-    filePath := filepath.Join(userPath, fileName)
-    err := saveFile(filePath, req.BinaryFile)
-    if err != nil {
-        return "", fmt.Errorf("failed to upload file: %w", err)
-    }
+	// Guardar el archivo en la ruta principal
+	filePath := filepath.Join(userPath, fileName)
+	err := saveFile(filePath, req.BinaryFile)
+	if err != nil {
+		return "", fmt.Errorf("failed to upload file: %w", err)
+	}
 
-    log.Printf("Archivo guardado en: %s", filePath)
-    return filePath, nil
 }
 
 func saveFile(filePath string, binaryFile []byte) error {
-    fileUpload, err := os.Create(filePath)
-    if err != nil {
-        return fmt.Errorf("failed to create file: %w", err)
-    }
-    defer fileUpload.Close()
+	fileUpload, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer fileUpload.Close()
 
-    decodedContent, err := base64.StdEncoding.DecodeString(string(binaryFile))
-    if err != nil {
-        return fmt.Errorf("failed to decode binary content: %w", err)
-    }
+	// Decodificar el contenido del binario que viene en base64
+	decodedContent, err := base64.StdEncoding.DecodeString(string(binaryFile))
+	if err != nil {
+		return fmt.Errorf("failed to decode binary content: %w", err)
+	}
 
-    if _, err = fileUpload.Write(decodedContent); err != nil {
-        return fmt.Errorf("failed to write binary content to file: %w", err)
-    }
+	// Escribir el contenido decodificado en el archivo
+	_, err = fileUpload.Write(decodedContent)
+	if err != nil {
+		return fmt.Errorf("failed to write binary content to file: %w", err)
+	}
 
-    log.Printf("Contenido decodificado escrito en el archivo: %s", filePath)
-    return nil
+	return nil
 }
+
+
